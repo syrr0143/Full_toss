88f58972f6ab259fe037a6e7af95b16531721e8f
syrr0143 <sy816120@gmail.com>
2024-12-04 19:29:10 +0530

    updated

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,13 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.jsx";
+// import { StrictMode } from "react";
+// import { createRoot } from "react-dom/client";
+// import "./index.css";
+// import App from "./App.jsx";
 
-import { ThemeProvider, useTheme } from "./context/ThemeContext";
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <ThemeProvider initialTeam="RCB">
-      <App />
-    </ThemeProvider>
-  </StrictMode>
-);
+// import { ThemeProvider, useTheme } from "./context/ThemeContext";
+// createRoot(document.getElementById("root")).render(
+//   <StrictMode>
+//     <ThemeProvider initialTeam="RCB">
+//       <App />
+//     </ThemeProvider>
+//   </StrictMode>
+// );
